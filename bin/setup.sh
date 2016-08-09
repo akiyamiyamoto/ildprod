3883bf859d4c1608c41b60012b4c1ca0381ebcf4
Akiya Miyamoto <akiya.miyamoto@kek.jp>
2016-08-09 12:37:43 +0900

    File name and directory name convention is written donw.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,5 @@
 
-export MCPROD_TOP=/home/ilc/miyamoto/DiracProd
+export MCPROD_TOP=~/ILDProd
 export PATH=${MCPROD_TOP}/bin:${PATH}
 export PYTHONPATH=${MCPROD_TOP}/bin:${PYTHONPATH}
 export MCPROD_CONFIG=${MCPROD_TOP}/config
