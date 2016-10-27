aed8d73f7091681cd7409ab47c2886f712ae5d53
akiyamiyamoto <akiya.miyamoto@kek.jp>
2016-10-27 09:48:07 +0900

    Test commit

---
+++ examples/marlin-with-upload/upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# a script to upload file
+#
+/bin/ls -l 
+dirac-dms-add-file -ddd /ilc/user/a/amiyamot/myprod2/test/dst/toto-25.dst.slcio toto-25.dst.slcio CERN-SRM
+dirac-dms-add-file -ddd /ilc/user/a/amiyamot/myprod2/test/rec/toto-25.rec.slcio toto-25.rec.slcio CERN-SRM
